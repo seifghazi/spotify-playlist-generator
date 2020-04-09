df227ea02173786504a419c8896a5dfa386bfad7
Seif Ghazi <seifghazi@gmail.com>
2020-04-08 21:37:32 -0400

    Kafka streams initial implementation

---
+++ tracks-processing/src/main/java/PopularityFilterApp.java
@@ -0,0 +1,86 @@
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Deserializer;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.Serializer;
+import org.apache.kafka.common.utils.Bytes;
+import org.apache.kafka.connect.json.JsonDeserializer;
+import org.apache.kafka.connect.json.JsonSerializer;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.streams.state.KeyValueStore;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Properties;
+
+public class PopularityFilterApp {
+
+    public static void main(String[] args) {
+        Properties config = new Properties();
+
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "spotify-playlist-gen-app");
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // Exactly once processing!!
+        config.put(StreamsConfig.PROCESSING_GUARANTEE_CONFIG, StreamsConfig.EXACTLY_ONCE);
+
+        // json Serde
+        final Serializer<JsonNode> jsonSerializer = new JsonSerializer();
+        final Deserializer<JsonNode> jsonDeserializer = new JsonDeserializer();
+        final Serde<JsonNode> jsonSerde = Serdes.serdeFrom(jsonSerializer, jsonDeserializer);
+
+        StreamsBuilder builder = new StreamsBuilder();
+
+        KStream<String, JsonNode> playlistTracks = builder.stream("playlist-tracklist",
+                Consumed.with(Serdes.String(), jsonSerde));
+
+
+        // create the initial json object for balances
+//        ObjectNode initialBalance = JsonNodeFactory.instance.objectNode();
+//        initialBalance.put("count", 0);
+//        initialBalance.put("balance", 0);
+//        initialBalance.put("time", Instant.ofEpochMilli(0L).toString());
+
+        KStream<String, JsonNode>[] branches = playlistTracks.branch(
+                (k, track) -> isPopular(track),
+                (k, track) -> true
+        );
+
+//        bankBalance.toStream().to("bank-balance-exactly-once", Produced.with(Serdes.String(), jsonSerde));
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), config);
+        streams.cleanUp();
+        streams.start();
+
+        // print the topology
+        streams.localThreadsMetadata().forEach(data -> System.out.println(data));
+
+        // shutdown hook to correctly close the streams application
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+    }
+
+    private static boolean isPopular(JsonNode playlistTracks) {
+        try {
+            System.out.println(playlistTracks);
+            JsonNode tracks = new ObjectMapper().readTree(playlistTracks.toString()).get("items");
+            System.out.println(tracks);
+            for (JsonNode track: tracks) {
+                int popularity = track.get("track").get("popularity").asInt();
+                System.out.println("POPULARITY: " + popularity);
+            }
+        } catch (Exception e) {
+        }
+        return false;
+    }
+}
