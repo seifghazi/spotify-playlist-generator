7d14837c0f458b248ee036c31e0f133521974c8c
Seif Ghazi <seifghazi@gmail.com>
2020-04-09 23:37:32 -0400

    Refator: Extract auth logic for producer into separate package

--- track_producer/tracks_producer.go
+++ track_producer/tracks_producer.go
@@ -4,10 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 
 	"github.com/Shopify/sarama"
+	"github.com/spotify-playlist-generator/track_producer/auth"
 	"github.com/zmb3/spotify"
 )
 
@@ -18,8 +18,8 @@ type App struct {
 const redirectURI = "http://localhost:8080/callback"
 
 var (
-	app    = &App{}
-	auth   = spotify.NewAuthenticator(redirectURI, spotify.ScopeUserReadPrivate)
+	app = &App{}
+	// auth   = spotify.NewAuthenticator(redirectURI, spotify.ScopeUserReadPrivate)
 	ch     = make(chan *spotify.Client)
 	state  = "abc123"
 	tracks = make(chan spotify.FullTrack)
@@ -33,10 +33,13 @@ func main() {
 	}
 
 	// authenticates user and stores tokens
-	err = Authenticate()
+	client, err := auth.Authenticate()
+
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	app.client = client
 
 	producer, err := KafkaProducerSetup()
 	if err != nil {
@@ -56,7 +59,7 @@ func main() {
 		}
 
 		_, _, err = producer.SendMessage(&sarama.ProducerMessage{
-			Topic: "playlist-tracklist",
+			Topic: "playlist-tracks",
 			Key:   sarama.StringEncoder(track.SimpleTrack.ID),
 			Value: sarama.StringEncoder(string(encodedTrack)),
 		})
@@ -66,31 +69,6 @@ func main() {
 		}
 	}
 
-}
-
-func Authenticate() error {
-	http.HandleFunc("/callback", completeAuth)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Got request for:", r.URL.String())
-	})
-	go http.ListenAndServe(":8080", nil)
-
-	url := auth.AuthURL(state)
-	fmt.Println("Please log in to Spotify by visiting the following page in your browser:", url)
-
-	// wait for auth to complete
-	client := <-ch
-	app.client = client
-
-	// use the client to make calls that require authorization
-	user, err := client.CurrentUser()
-	if err != nil {
-		return err
-	}
-
-	fmt.Println("You are logged in as:", user.ID)
-
-	return nil
 }
 
 // GetPlaylistTracks gets tracks belonging to a playlist
@@ -152,23 +130,4 @@ func loadConfig() (map[string]interface{}, error) {
 	}
 
 	return config, nil
-}
-
-func completeAuth(w http.ResponseWriter, r *http.Request) {
-	tok, err := auth.Token(state, r)
-
-	if err != nil {
-		http.Error(w, "Couldn't get token", http.StatusForbidden)
-		log.Fatal(err)
-	}
-
-	if st := r.FormValue("state"); st != state {
-		http.NotFound(w, r)
-		log.Fatalf("State mismatch: %s != %s\n", st, state)
-	}
-
-	// use the token to get an authenticated client
-	client := auth.NewClient(tok)
-	fmt.Fprintf(w, "Login Completed!")
-	ch <- &client
 }
